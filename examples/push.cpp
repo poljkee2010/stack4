bd1cbad0f47b71752301761f156770181b8f28ec
poljkee2010 <31631937+poljkee2010@users.noreply.github.com>
2017-10-15 21:37:36 +0300

    Update push.cpp

--- examples/push.cpp
+++ examples/push.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include "stack.hpp"
+using namespace std;
 
+void StackPop()
+{
+	try {
+		cout << "File pop.cpp" << endl;
+		stack<size_t>sizetStack;
+		sizetStack.push(1);
+		cout << "Content file pop.cpp\t" << sizetStack.pop() << endl;
+	}
+
+	catch (const exception& e)
+	{
+		cout << e.what() << endl;
+	}
+}
+
+int main()
+{
+	StackPop();
+	return 0;
+}
