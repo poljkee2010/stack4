a68e2372efb646f213188b817e7242446912b6dd
poljkee2010 <31631937+poljkee2010@users.noreply.github.com>
2017-10-15 21:36:48 +0300

    Update empty.cpp

--- examples/empty.cpp
+++ examples/empty.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include "stack.hpp"
+using namespace std;
 
+void StackEmpty()
+{
+	try {
+		cout << "File empty.cpp" << endl;
+		stack<float>FloatSt;
+		for (int i = 0; i != 10; ++i)
+		{
+			FloatSt.push(i);
+		}
+
+		while (!FloatSt.empty())
+		{
+			cout << FloatSt.pop() << " " << endl;
+		}
+		cout << "File  empty.cpp have 0 elements" << endl;
+	}
+
+		catch (const exception& e) //Обработка исключений
+		{
+			cout << e.what() << endl;
+		}
+}
+	
+int main()
+{
+		StackEmpty();
+		return 0;
+}
