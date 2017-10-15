2652888fd796d758af7c61a9078ce75a802d7e3a
poljkee2010 <31631937+poljkee2010@users.noreply.github.com>
2017-10-15 21:37:13 +0300

    Update init.cpp

--- examples/init.cpp
+++ examples/init.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "stack.hpp"
+using namespace std;
 
+void StackInit()
+{
+	cout << "File init.cpp" << endl;
+	stack<int>intStack;
+	cout << "Created intStack" << endl;
+}
+
+int main()
+{
+	StackInit();
+	return 0;
+}
